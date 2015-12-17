7053ccd023b92ee0bf1fec641b0c7fe72faffc9a
Nader Nour <nnmh1992@hotmail.com>
2015-12-17 17:06:03 +0200

    Added CountDown scripts and learned about Variable And Events

---
+++ Source/MiniTuts/Countdown.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Countdown.generated.h"
+
+UCLASS()
+class MINITUTS_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountdown();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// How long in seconds, the countdown will run
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CountdownTime;
+
+	UPROPERTY(BlueprintReadWrite)
+	UTextRenderComponent* CountdownText;
+
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished();
+	virtual void CountdownHasFinished_Implementation();
+
+	FTimerHandle CountdownTimerHandle;
+};
